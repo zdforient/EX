f5f2e6c5b5a47cc00b1f78266e2a4f10b7b20772
DONGFANG <zdforient@gmail.com>
2021-02-08 23:34:15 -0500

    rgbd

---
+++ 5/map/depth/build/CMakeFiles/rgbd.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zdf/Documents/GITHUB/EX_SLAM_Practice/5/map/depth/rgbd.cpp" "/home/zdf/Documents/GITHUB/EX_SLAM_Practice/5/map/depth/build/CMakeFiles/rgbd.dir/rgbd.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "/home/zdf/Documents/pkgs/Pangolin/include"
+  "/home/zdf/Documents/pkgs/Pangolin/build/src/include"
+  "/usr/include/eigen3"
+  "/home/zdf/Documents/pkgs/Sophus"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 5/map/depth/build/CMakeFiles/rgbd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rgbd.dir/rgbd.cpp.o"
+  "rgbd.pdb"
+  "rgbd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rgbd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
