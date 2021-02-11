55aac209362ece972a5c2af9d016c6da33870343
DONGFANG <zdforient@gmail.com>
2021-02-11 16:04:02 -0500

    2D_2D

---
+++ 7/2d-2d/build/CMakeFiles/2D_2D.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2D_2D.dir/2D_2D.cpp.o"
+  "2D_2D.pdb"
+  "2D_2D"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2D_2D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
